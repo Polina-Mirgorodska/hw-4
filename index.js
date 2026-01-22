2ec15674085bbccafd10b9b5dcb3d56ad1e8fc7d
Polina-Mirgorodska <mirgorodskapolina@gmail.com>
2026-01-23 00:15:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const word1 = "";
+const word2 = "";
+
+if(word1 !== "" && word2 !== ""){
+    console.log("Обидва поля заповнені");
+} else {
+    console.log("Не всі поля заповнені");
+};
+
+const num1 = 12;
+const num2 = 4;
+const suma = num1 + num2;
+
+if(suma > 10){
+    console.log("Сума більша за 10");
+} else {
+    console.log("Сума менша або дорівнює 10");
+};
+
